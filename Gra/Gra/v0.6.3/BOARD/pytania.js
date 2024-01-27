4a473015b18ea710b0980b662965ad7725088a8f
Neplep <153120692+Neplep@users.noreply.github.com>
2024-01-27 17:44:39 +0100

    Add files via upload

---
+++ Gra/Gra/v0.6.3/BOARD/pytania.js
@@ -0,0 +1,361 @@
+const zawodowe=[
+{
+    idx:1,
+    tresc: "Które wielkości należy mierzyć w celu wyznaczenia zakresu liniowości wzmacniacza?",
+    odpa: "Napięcie wejściowe i moc wyjściową.",
+    odpb: "Napięcie wyjściowe i częstotliwość.",
+    odpc: "Napięcie wyjściowe i napięcie zasilania.",
+    odpd: "Napięcie wejściowe i wyjściowe.",
+    prawidlowaodp: "D"
+},
+{
+    idx:2,
+    tresc: "Podczas sprawdzania instalacji domofonu cyfrowego, po podłączeniu napięcia zasilającego okazało się, że w słuchawce słychać piski i rozmowa jest słabo słyszalna. Aby usunąć usterkę należy",
+    odpa: "obniżyć poziom głośności wywołania w unifonie.",
+    odpb: "podwyższyć napięcie zasilania elektrozaczepu.",
+    odpc: "wyczyścić przyciski w kasecie rozmownej.",
+    odpd: "wyregulować poziom głośności w centrali.",
+    prawidlowaodp: "D"
+},
+{
+    idx:3,
+    tresc: "Podłączenie po jednej stronie toru transmisyjnego źródła sygnału o stałej i znanej mocy, a po drugiej miernika mocy optycznej, umożliwia bezpośrednie określenie.",
+    odpa: "tłumienia złączy.",
+    odpb: "długości światłowodu.",
+    odpc: "miejsca spawu lub zgięcia włókna światłowodowego.",
+    odpd: "całkowitego tłumienia toru optycznego.",
+    prawidlowaodp: "D"
+},
+{
+    idx:4,
+    tresc: "Do sprawdzenia ciągłości połączeń w obwodach drukowanych urządzeń elektronicznych należy użyć.",
+    odpa: "omomierza.",
+    odpb: "watomierza.",
+    odpc: "woltomierza.",
+    odpd: "amperomierza.",
+    prawidlowaodp: "A"
+},
+{
+    idx:5,
+    tresc: "Która technologia wykorzystuje światło podczerwone do transmisji danych?",
+    odpa: "IRDA",
+    odpb: "BLUETOOTH",
+    odpc: "ZIGBEE",
+    odpd: "WIMAX",
+    prawidlowaodp: "A"
+},
+{
+    idx:6,
+    tresc: "Prawidłowa kolejność czynności przy wymianie uszkodzonej czujki ruchu instalacji kontroli dostępu jest następująca:",
+    odpa: "wpisać kod serwisowy, odłączyć akumulator, wymienić czujkę.",
+    odpb: "wpisać kod serwisowy, odłączyć zasilanie AC, odłączyć akumulator, wymienić czujkę.",
+    odpc: "wpisać kod użytkownika, odłączyć zasilanie AC, wymienić czujkę.",
+    odpd: "wpisać kod użytkownika, odłączyć zasilanie AC, odłączyć akumulator, wymienić czujkę.",
+    prawidlowaodp: "B"
+},
+{
+    idx:7,
+    tresc: "Która z poniższych liczb nie jest zapisem w kodzie BCD8421?",
+    odpa: "11111111",
+    odpb: "01100110",
+    odpc: "00000000",
+    odpd: "10011001",
+    prawidlowaodp: "A"
+},
+{
+    idx:8,
+    tresc: "Na podstawie oględzin instalacji telewizyjnej nie można określić:",
+    odpa: "uszkodzenia powłoki kabla.", odpb: "skorodowania czaszy anteny.",
+    odpc: "uszkodzenia elektroniki konwertera.",
+    odpd: "zniekształcenia lustra czaszy anteny.",
+    prawidlowaodp: "C"
+},
+{
+    idx:9,
+    tresc: "Zgodnie z normą TIA/EIA-568-B.1 kabel UTP 5e z przeplotem jest tworzony poprzez zamianę ułożenia we wtyczce 8P8C następujących par przewodów (odpowiednio według kolorów):",
+    odpa: "biało-pomarańczowy i pomarańczowy z biało-zielonym i zielonym.",
+    odpb: "biało-zielony i zielony z biało-niebieskim i niebieskim.",
+    odpc: "biało-zielony i zielony z biało-brązowym i brązowym.",
+    odpd: "biało-pomarańczowy i pomarańczowy z biało-brązowym i brązowym.",
+    prawidlowaodp: "A"
+},
+{
+    idx:10,
+    tresc: "Urządzeniem, które umożliwia komputerom bezprzewodowy dostęp do przewodowej sieci komputerowej, jest:",
+    odpa: "punkt dostępu.",
+    odpb: "modem.",
+    odpc: "regenerator.",
+    odpd: "koncentrator.",
+    prawidlowaodp: "A"
+},
+{
+    idx:11,
+    tresc: "Host automatycznie pobiera adres IP z serwera DHCP. W przypadku gdy serwer DHCP ulegnie uszkodzeniu, karcie sieciowej zostanie przypisany adres IP z zakresu:",
+    odpa: "192.168.0.1 ÷ 192.168.255.254",
+    odpb: "169.254.0.1 ÷ 169.254.255.254",
+    odpc: "127.0.0.1 ÷ 127.255.255.255.254",
+    odpd: "224.0.0.1 ÷ 224.255.255.254",
+    prawidlowaodp: "B"
+},
+{
+    idx:12,
+    tresc: "Które polecenie systemu z rodziny Windows pozwala sprawdzić, przechowywane w pamięci podręcznej komputera, zapamiętane tłumaczenia nazw DNS na adresy IP?",
+    odpa: "ipconfig /flushdns",
+    odpb: "ipconfig /release",
+    odpc: "ipconfig /displaydns",
+    odpd: "ipconfig /renew",
+    prawidlowaodp: "B"
+},
+{
+    idx:13,
+    tresc: "Typ transmisji danych do jednego bądź kilku komputerów w jednej chwili, w której odbiorcy są widziani dla nadawcy jako pojedynczy grupowy odbiorca, to:",
+    odpa: "unicast.",
+    odpb: "broadcast.",
+    odpc: "anycast.",
+    odpd: "multicast.",
+    prawidlowaodp: "D"
+},
+{
+    idx:14,
+    tresc: "Oprogramowanie do wirtualizacji dostępne jako rola w systemie Windows Serwer 2012 to:",
+    odpa: "Hyper-V",
+    odpb: "VMware",
+    odpc: "Virtual Box",
+    odpd: "Virtual PC",
+    prawidlowaodp: "A"
+},
+{
+    idx:15,
+    tresc: "Ile maksymalnie adresów można przydzielić hostom w sieci 10.0.0.0/22?",
+    odpa: "512 adresów.",
+    odpb: "510 adresów.",
+    odpc: "1 022 adresy.",
+    odpd: "1 024 adresy.",
+    prawidlowaodp: "C"
+},
+{
+    idx:16,
+    tresc: "Aby zabezpieczyć komunikację terminalową z serwerem, należy użyć połączenia z wykorzystaniem protokołu:",
+    odpa: "Telnet",
+    odpb: "SSH",
+    odpc: "SFTP",
+    odpd: "TFTP",
+    prawidlowaodp: "B"
+},
+{
+    idx:17,
+    tresc: "Wskaż znacznik pozwalający na zapisanie tekstu nieprawidłowego lub nieodpowiedniego w sposób przekreślony.",
+    odpa: "<s> </s>",
+    odpb: "<b> </b>",
+    odpc: "<em> </em>",
+    odpd: "<sub> </sub>",
+    prawidlowaodp: "A"
+},
+{
+    idx:18,
+    tresc: "Znacznik <pre> </pre> jest stosowany w celu wyświetlenia:",
+    odpa: "znaku przekreślenia.",
+    odpb: "znaku wielokropka.",
+    odpc: "treści polską czcionką.",
+    odpd: "treści czcionką o stałej szerokości.",
+    prawidlowaodp: "D"
+},
+{
+    idx:19,
+    tresc: "W znaczniku meta w miejsce kropek należy wpisać:  <meta name='description' content='…'>",
+    odpa: "nazwę edytora.",
+    odpb: "język dokumentu.",
+    odpc: "streszczenie treści strony.",
+    odpd: "informację o dostosowaniu do urządzeń mobilnych.",
+    prawidlowaodp: "C"
+},
+{
+    idx:20,
+    tresc: "W której części dokumentu HTML należy umieścić wewnętrzny arkusz stylów?",
+    odpa: "W ciele strony.",
+    odpb: "W części nagłówkowej strony.",
+    odpc: "W skrypcie dołączonym do strony.",
+    odpd: "Wewnątrz znacznika, którego styl dotyczy.",
+    prawidlowaodp: "B"
+},
+{
+    idx:21,
+    tresc: "Aby dołączyć kaskadowy arkusz stylów zapisany w zewnętrznym pliku, należy użyć następującego fragmentu kodu HTML",
+    odpa: '<meta charset="styl.css" />',
+    odpb: '<div id="styl.css" relation="css" />',
+    odpc: '<optionvalue="styl.css" type="text/css" />',
+    odpd: '<link rel="stylesheet" type="text/css" href="styl.css" />',
+    prawidlowaodp: "D"
+},
+{
+    idx:22,
+    tresc: "Głównym celem stosowania edytora WYSIWYG jest",
+    odpa: "szybka wizualizacja tworzonej strony.",
+    odpb: "wyszukiwanie błędów w bazie danych.",
+    odpc: "pobieranie z Internetu kompletnych portali WWW.",
+    odpd: "zautomatyzowanie odtwarzania plików multimedialnych.",
+    prawidlowaodp: "A"
+},
+{
+    idx:23,
+    tresc: "Aby obsłużyć połączenie z bazą MySQL podczas tworzenia aplikacji internetowej, można wykorzystać język",
+    odpa: "CSS",
+    odpb: "PHP",
+    odpc: "HTML",
+    odpd: "XHTML",
+    prawidlowaodp: "B"
+},
+{
+    idx:24,
+    tresc: "Aby wskazać błędy składniowe w kodzie HTML, należy zastosować",
+    odpa: "debugger.",
+    odpb: "walidator.",
+    odpc: "interpreter.",
+    odpd: "kompilator.",
+    prawidlowaodp: "B"
+},
+{
+    idx:25,
+    tresc: "W celu obliczenia przepływności strumienia cyfrowego generowanego przez pojedynczą rozmowę telefoniczną należy liczbę bitów przypadających na jedną próbkę pomnożyć przez",
+    odpa: "częstotliwość próbkowania.",
+    odpb: "częstotliwość pasma telefonicznego.",
+    odpc: "dolną częstotliwość pasma telefonicznego.",
+    odpd: "górną częstotliwość pasma telefonicznego.",
+    prawidlowaodp: "A"
+},
+{
+    idx:26,
+    tresc: "Pasmo fal radiowych oznaczone skrótem UHF (Ultra High Frequency) obejmuje zakres częstotliwości",
+    odpa: "30 MHz ÷ 300 MHz",
+    odpb: "3 MHz ÷ 30 MHz",
+    odpc: "3 000 MHz ÷ 30 000 MHz",
+    odpd: "300 MHz ÷ 3 000 MHz",
+    prawidlowaodp: "D"
+},
+{
+    idx:27,
+    tresc: "Jak jest oznaczona odmiana szerokopasmowego systemu telekomunikacyjnego FTTX (Fiber-To-The-X), w którym światłowód jest doprowadzony do mieszkania abonenta?",
+    odpa: "FTTH",
+    odpb: "FTTB",
+    odpc: "FTTN",
+    odpd: "FTTC",
+    prawidlowaodp: "A"
+},
+{
+    idx:28,
+    tresc: "W celu umożliwienia dostępu do Internetu komputerom, tabletom i innym urządzeniom w domu lub mieszkaniu należy zastosować ruter",
+    odpa: "szkieletowy.",
+    odpb: "dostępowy.",
+    odpc: "brzegowy.",
+    odpd: "korporacyjny.",
+    prawidlowaodp: "B"
+},
+{
+    idx:29,
+    tresc: "W którym medium transmisyjnym przesyłany sygnał jest najmniej narażony na zakłócenia radioelektryczne?",
+    odpa: "W kablu koncentrycznym.",
+    odpb: "W kablu światłowodowym.",
+    odpc: "W skrętce komputerowej ekranowanej.",
+    odpd: "W skrętce komputerowej nieekranowanej.",
+    prawidlowaodp: "B"
+},
+{
+    idx:30,
+    tresc: "Wskaż element sieci telefonii komórkowej GSM, który nie bierze udziału w zestawianiu połączeń między abonentami tej sieci, niekorzystającymi z usług sieci inteligentnych.",
+    odpa: "VLR (Visitor Location Register)",
+    odpb: "SCP (Service Control Point)",
+    odpc: "MSC (Mobile Switching Centre)",
+    odpd: "HLR (Home Location Register)",
+    prawidlowaodp: "B"
+},
+{
+    idx:31,
+    tresc: "Kabel optyczny oznaczony symbolem Z-XOTKtsd 16J ma powłokę zewnętrzną wykonaną",
+    odpa: "z poliuretanu.",
+    odpb: "z polwinitu.",
+    odpc: "z poliamidu.",
+    odpd: "z polietylenu.",
+    prawidlowaodp: "D"
+},
+{
+    idx:32,
+    tresc: "Usługa dodatkowa w sieci ISDN oznaczona skrótem CFNR (Call Forwarding No Reply) umożliwia przekierowanie połączenia, gdy abonent wywoływany",
+    odpa: "jest zajęty.",
+    odpb: "nie odpowiada.",
+    odpc: "jest nieosiągalny.",
+    odpd: "ma załączoną usługę DND.",
+    prawidlowaodp: "B"
+},
+{
+    idx:33,
+    tresc: "Obiektyw, wizjer, lampa błyskowa, migawka, układ zasilania to elementy budowy",
+    odpa: "analogowego odtwarzacza fonograficznego.",
+    odpb: "kamery internetowej.",
+    odpc: "cyfrowej kamery wideo.",
+    odpd: "cyfrowego aparatu fotograficznego.",
+    prawidlowaodp: "D"
+},
+{
+    idx:34,
+    tresc: "Który panel nie występuje w programie do obróbki grafiki rastrowej?",
+    odpa: "Mikser.",
+    odpb: "Ścieżki.",
+    odpc: "Próbki.",
+    odpd: "Kanały.",
+    prawidlowaodp: "A"
+},
+{
+    idx:35,
+    tresc: "W której rozdzielczości i w trybie koloru należy przygotować obraz cyfrowy przeznaczony do zamieszczania w prezentacji multimedialnej publikowanej w internecie?",
+    odpa: "72 ppi, RGB",
+    odpb: "300 ppi, RGB",
+    odpc: "72 ppi, CMYK",
+    odpd: "300 ppi, CMYK",
+    prawidlowaodp: "A"
+},
+{
+    idx:36,
+    tresc: "W którym trybie pracy aparatu fotograficznego można dokonać rejestracji obrazu z preselekcją czasu naświetlania?",
+    odpa: "Auto",
+    odpb: "P",
+    odpc: "Tv",
+    odpd: "Av",
+    prawidlowaodp: "C"
+},
+{
+    idx:37,
+    tresc: "Jak nazywa się specjalny adres, który posiada każda opublikowana strona i zasób internetowy?",
+    odpa: "IMAP",
+    odpb: "URL",
+    odpc: "FTP",
+    odpd: "IP",
+    prawidlowaodp: "B"
+},
+{
+    idx:38,
+    tresc: "Które oprogramowanie pozwala na tworzenie jednego slajdu w drugim techniką zoomowania z wykorzystaniem tzw. mapy myśli?",
+    odpa: "Paint 3D",
+    odpb: "Corel Draw",
+    odpc: "Prezi Desktop",
+    odpd: "LibreOffice Impress",
+    prawidlowaodp: "C"
+},
+{
+    idx:39,
+    tresc: "Obraz o walorach odwróconych i barwach dopełniających w stosunku do oryginału to",
+    odpa: "odbitka fotograficzna.",
+    odpb: "diapozytyw.",
+    odpc: "negatyw.",
+    odpd: "slajd.",
+    prawidlowaodp: "C"
+},
+{
+    idx:40,
+    tresc: "W którym trybie koloru należy zapisać fotografię cyfrową, która ma zostać wykorzystana w publikacji przygotowywanej przez drukarnię offsetową?",
+    odpa: "RGB",
+    odpb: "HSL",
+    odpc: "CMYK",
+    odpd: "Lab",
+    prawidlowaodp: "C"
+},
+];
